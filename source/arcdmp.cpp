00e457fba0cb5ab5b71b88bf62476d918311defc
Jason <jst@it2tms.com>
2014-01-02 13:01:10 +0000

    Remove hardcoded executable extensions and rely on detect of magic 'MZ' flag. Reduce scope on some locals, remove some useless comments.

--- source/arcdmp.cpp
+++ source/arcdmp.cpp
@@ -1,32 +1,29 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-//stl
 #include <iostream>
 #include <fstream>
 #include <vector>
 using namespace std;
 
-//boost
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <boost/algorithm/string.hpp>
-
 using namespace boost;
 using namespace boost::filesystem;
 using namespace boost::program_options;
 
-string GetMachineType(string file_in);
+void OutputMachineTypes(string directory, string arcFilter, string fileFilter);
 
 int main(int argc, char* argv[])
 {
   options_description desc("Allowed options");
   desc.add_options()
-      ("help", "Displays target platform of executables in a directory")
-      ("filter", value<string>(), "Filter to apply. (arg = x86, x64 or ANYCPU)")
-      ("directory", value<string>(), "The target directory. (arg defaults to current working directory)")
-	    ("file", value<string>(), "Display only results which match this filename")
-      ;
+    ("help", "Displays target platform of executables in a directory")
+    ("directory", value<string>(), "The target directory. (defaults to current working directory)")
+    ("arc", value<string>(), "Filter by architecture. (x86, x64, ANYCPU or Itanium)")
+    ("filename", value<string>(), "Filter by filename.")
+    ;
 
   try
   {
@@ -41,32 +38,18 @@ int main(int argc, char* argv[])
     }
 
     string arcFilter;
-    if(varMap.count("filter"))
-      arcFilter = varMap["filter"].as<string>();
+    if(varMap.count("arc"))
+      arcFilter = to_upper_copy(varMap["arc"].as<string>());
 
-	  string fileFilter;
-	  if(varMap.count("file"))
-		  fileFilter = varMap["file"].as<string>();
+    string fileFilter;
+    if(varMap.count("filename"))
+      fileFilter = to_upper_copy(varMap["filename"].as<string>());
 
-    //extract target directory if supplied
     string target_dir = current_path().string();
     if(varMap.count("directory"))
       target_dir = varMap["directory"].as<string>();
 
-    directory_iterator end;
-    for(directory_iterator iter(target_dir); iter != end; ++iter)
-    {
-      string location = iter->path().string();
-      string filename = iter->path().filename().string();
-      string extension = to_upper_copy(iter->path().extension().string());
-
-      if(extension == ".DLL" || extension == ".EXE")
-      {
-        string architecture = GetMachineType(location);
-        if((arcFilter.empty() || architecture == arcFilter) && (fileFilter.empty() || fileFilter == filename) )
-          cout << filename << " (" << architecture << ")" << endl;
-      }
-    } 
+    OutputMachineTypes(target_dir, arcFilter, fileFilter);
   }
   catch(const std::exception& e)
   {
@@ -74,97 +57,104 @@ int main(int argc, char* argv[])
     cout << desc << endl;
   }
 
-	return 0;
+  return 0;
 }
 
-string GetMachineType(string file_in)
+void OutputMachineTypes(string directory, string arcFilter, string fileFilter)
 {
-	string toReturn = "???";
-  fstream file_stream = fstream(file_in, ios::in | ios::binary);
-
-  file_stream.seekg(0, ios::beg);
-  if(file_stream.is_open())
+  directory_iterator end;
+  for(directory_iterator iter(directory); iter != end; ++iter)
   {
-    //read the dos header
-		IMAGE_DOS_HEADER dosHeader ={};
-    file_stream.read((char*)&dosHeader, sizeof(IMAGE_DOS_HEADER));
+    fstream file_stream = fstream(iter->path().string(), ios::in | ios::binary);
+    file_stream.seekg(0, ios::beg);
 
-    //seek to the start of IMAGE_NT_HEADERS (skipping the Signature)
-    file_stream.seekg(dosHeader.e_lfanew + sizeof(DWORD));
-
-    //read the NTHeaders signature
-		IMAGE_FILE_HEADER fileHeader ={};
-    file_stream.read((char*)&fileHeader, sizeof(fileHeader));   
-    
-    //read into the correct structure
-		IMAGE_OPTIONAL_HEADER64 nt64OptHeader ={};
-		IMAGE_OPTIONAL_HEADER32 nt32OptHeader ={};
-		IMAGE_DATA_DIRECTORY clrHeaderLocation ={};
-    if(sizeof(IMAGE_OPTIONAL_HEADER64) == fileHeader.SizeOfOptionalHeader)
+    if(file_stream.is_open())
     {
-      file_stream.read((char*)&nt64OptHeader, fileHeader.SizeOfOptionalHeader);
-      clrHeaderLocation = nt64OptHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR];
-    }
-    else if(sizeof(IMAGE_OPTIONAL_HEADER32) == fileHeader.SizeOfOptionalHeader)
-    {
-      file_stream.read((char*)&nt32OptHeader, fileHeader.SizeOfOptionalHeader);
-      clrHeaderLocation = nt32OptHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR];
-    }
+      //read the dos header
+      IMAGE_DOS_HEADER dosHeader ={};
+      file_stream.read((char*)&dosHeader, sizeof(IMAGE_DOS_HEADER));
 
-    bool isManaged = (clrHeaderLocation.VirtualAddress != 0);
+      const bool isExecutable = (dosHeader.e_magic == IMAGE_DOS_SIGNATURE);
+      if(isExecutable)
+      {
+        //seek to the start of IMAGE_NT_HEADERS (skipping the Signature)
+        file_stream.seekg(dosHeader.e_lfanew + sizeof(DWORD), ios::beg);
 
-    //For native executables we can tell its x64 by looking at fileHeader.Machine
-    //For managed executables:
-		//									If its compiled for x64 we also can tell by fileHeader.Machine
-		//									To determine whether its x86 | ANY CPU we need the 32BITREQ flag, which lives in the CLR header
+        //read the NTHeaders signature
+        IMAGE_FILE_HEADER fileHeader ={};
+        file_stream.read((char*)&fileHeader, sizeof(fileHeader));   
 
-		IMAGE_COR20_HEADER clrHeader ={};
-		IMAGE_SECTION_HEADER sectClrHeaderIsIn ={};
-    if(isManaged)
-    { 
-      //find out which section header the clr header lives in;
-      //And when we do, continue to iterate the sections so that
-      //the read pointer in the file_stream is at the end of all the section headers
-      for(int i = 0; i < fileHeader.NumberOfSections; ++i)
-      { 
-        IMAGE_SECTION_HEADER currentSection ={};
-        file_stream.read((char*)&currentSection, sizeof(currentSection));
-        if(currentSection.VirtualAddress <= clrHeaderLocation.VirtualAddress && currentSection.VirtualAddress + currentSection.SizeOfRawData > clrHeaderLocation.VirtualAddress)
-          sectClrHeaderIsIn = currentSection;
+        //read into the correct structure
+        IMAGE_DATA_DIRECTORY clrHeaderLocation ={};
+        if(sizeof(IMAGE_OPTIONAL_HEADER64) == fileHeader.SizeOfOptionalHeader)
+        {
+          IMAGE_OPTIONAL_HEADER64 nt64OptHeader ={};
+          file_stream.read((char*)&nt64OptHeader, fileHeader.SizeOfOptionalHeader);
+          clrHeaderLocation = nt64OptHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR];
+        }
+        else if(sizeof(IMAGE_OPTIONAL_HEADER32) == fileHeader.SizeOfOptionalHeader)
+        {
+          IMAGE_OPTIONAL_HEADER32 nt32OptHeader ={};
+          file_stream.read((char*)&nt32OptHeader, fileHeader.SizeOfOptionalHeader);
+          clrHeaderLocation = nt32OptHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR];
+        }
+
+        //For native executables we can tell its x64 by looking at fileHeader.Machine
+        //For managed executables:
+        //									If its compiled for x64 we also can tell by fileHeader.Machine
+        //									To determine whether its x86 | ANY CPU we need the 32BITREQ flag, which lives in the CLR header
+
+        IMAGE_COR20_HEADER clrHeader ={};
+        const bool isManaged = (clrHeaderLocation.VirtualAddress != 0);
+        if(isManaged)
+        { 
+          //find out which section header the clr header lives in;
+          //And when we do, continue to iterate the sections so that
+          //the read pointer in the file_stream is at the end of all the section headers
+          IMAGE_SECTION_HEADER sectClrHeaderIsIn ={};
+          for(int i = 0; i < fileHeader.NumberOfSections; ++i)
+          { 
+            IMAGE_SECTION_HEADER currentSection ={};
+            file_stream.read((char*)&currentSection, sizeof(currentSection));
+            if(currentSection.VirtualAddress <= clrHeaderLocation.VirtualAddress && currentSection.VirtualAddress + currentSection.SizeOfRawData > clrHeaderLocation.VirtualAddress)
+              sectClrHeaderIsIn = currentSection;
+          }
+
+          //get the clr header
+          file_stream.seekg(sectClrHeaderIsIn.PointerToRawData + (clrHeaderLocation.VirtualAddress - sectClrHeaderIsIn.VirtualAddress), ios::beg);
+          file_stream.read((char*)&clrHeader, sizeof(IMAGE_COR20_HEADER));
+        }
+
+        file_stream.close();
+
+        string architecture;
+        switch(fileHeader.Machine)
+        {
+        case IMAGE_FILE_MACHINE_AMD64:
+          architecture = "X64";
+          break;
+
+        case IMAGE_FILE_MACHINE_I386:
+          if(isManaged)
+            architecture = ((clrHeader.Flags & COMIMAGE_FLAGS_32BITREQUIRED) > 0) ? "X86" : "ANYCPU";
+          else
+            architecture = "X86";
+          break;
+
+        case IMAGE_FILE_MACHINE_IA64:
+          architecture = "ITANIUM";
+          break;
+
+        case IMAGE_FILE_MACHINE_UNKNOWN:
+        default:
+          architecture = "???";
+          break;
+        }
+
+        const string filename = to_upper_copy(iter->path().filename().string());
+        if((arcFilter.empty() || architecture == arcFilter) && (fileFilter.empty() || fileFilter == filename) )
+          cout << iter->path().filename().string() << " (" << architecture << ")" << endl;
       }
-
-			//get the clr header
-			file_stream.seekg(sectClrHeaderIsIn.PointerToRawData + (clrHeaderLocation.VirtualAddress - sectClrHeaderIsIn.VirtualAddress), ios::beg);
-			file_stream.read((char*)&clrHeader, sizeof(IMAGE_COR20_HEADER));
-    }
-
-    file_stream.close();
-
-    switch(fileHeader.Machine)
-    {
-    case IMAGE_FILE_MACHINE_UNKNOWN:
-      toReturn = "Unknown";
-      break;
-
-    case IMAGE_FILE_MACHINE_AMD64:
-      toReturn = "x64";
-      break;
-
-    case IMAGE_FILE_MACHINE_I386:
-      if(isManaged)
-				toReturn = ((clrHeader.Flags & COMIMAGE_FLAGS_32BITREQUIRED) > 0) ? "x86" : "ANYCPU";
-			else
-				toReturn = "x86";
-      break;
-
-    case IMAGE_FILE_MACHINE_IA64:
-      toReturn = "Itanium";
-      break;
-
-    default:
-      break;
     }
   }
-
-  return toReturn;
 }
